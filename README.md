10c08871dbd54b65c9df5f687d8e8ec3813e5563
Helco <hermann.noll@hotmail.com>
2018-11-28 10:09:36 +0100

    first working version

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # github-project-workflow-bot
 
-> A GitHub App built with [Probot](https://github.com/probot/probot) that Automate your github project workflow more
+> A GitHub App built with [Probot](https://github.com/probot/probot) to Automate your github project workflow more.
+> Currently this means replacing an issue in a project for which a fixing pull request is opened.
 
 ## Setup
 
@@ -16,6 +17,8 @@ npm start
 ```
 
 ## Contributing
+
+> The code is bad and I should feel bad
 
 If you have suggestions for how github-project-workflow-bot could be improved, or want to report a bug, open an issue! We'd love all and any contributions.
 
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,95 @@
-import { Application } from 'probot' // eslint-disable-line no-unused-vars
+import { Application, Context } from 'probot' // eslint-disable-line no-unused-vars
+import { ProjectsGetCardParams } from '@octokit/rest';
+
+function extractIssueNumber(app: Application, payload: any): number {
+  if (typeof(payload) !== "object" ||
+      typeof(payload.pull_request) !== "object" ||
+      typeof(payload.pull_request.body) !== "string") {
+        app.log.error("Invalid pull request payload");
+        return -1;
+  }
+  const results = /[Ff]ixes\s+#(\d+)/.exec(payload.pull_request.body);
+  if (results != null)
+    return parseInt(results[1]);
+  return -1;
+}
+
+interface CardRef {
+  project_id: number;
+  column_id: number;
+  card_id: number;
+}
+
+async function findProjectCards(context: Context, issueNumber: number): Promise<CardRef[]> {
+  const projects = await context.github.projects.listForRepo(context.repo());
+  if (projects.status != 200)
+    return [];
+  var result: CardRef[] = [];
+  for (const project of projects.data) {
+    const columns = await context.github.projects.listColumns({ project_id: project.id });
+    if (columns.status != 200)
+      continue;
+    for (const column of columns.data) {
+      const cards = await context.github.projects.listCards({
+        column_id: column.id,
+        per_page: 100
+      });
+      if (cards.status != 200)
+        continue;
+      for (const card of cards.data) {
+        if (card.content_url.endsWith("/issues/" + issueNumber))
+        {
+          result.push({
+            project_id: project.id,
+            column_id: column.id,
+            card_id: card.id
+          });
+          break;
+        }
+      }
+    }
+  }
+  return result;
+}
 
 export = (app: Application) => {
-  app.on('issues.opened', async (context) => {
+  /*app.on('issues.opened', async (context) => {
     const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
     await context.github.issues.createComment(issueComment)
-  })
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+  });*/
+  app.on('pull_request', async (context) => {
+    app.log.info("Hi?");
+    const issueNumber = extractIssueNumber(app, context.payload);
+    if (issueNumber < 0)
+      return;
+    const issue = await context.github.issues.get(context.repo({number: issueNumber}));
+    if (issue.status !== 200) {
+      app.log.error("Did not find fixed issue");
+      return;
+    }
+    if (("pull_request" in issue.data) && issue.data.pull_request) {
+      app.log.error("Pull request is fixing a pull request");
+      return;
+    }
+    const cards = await findProjectCards(context, issueNumber);
+    if (cards.length == 0)
+      app.log.error("Did not found any cards");
+    for (const card of cards)
+    {
+      const didDelete = await context.github.projects.deleteCard({ card_id: card.card_id });
+      if (didDelete.status != 204) {
+        app.log.error("Could not delete issue " + didDelete.status);
+        return;
+      }
+      const didCreate = await context.github.projects.createCard({
+        column_id: card.column_id,
+        content_id: context.payload.pull_request.id,
+        content_type: "PullRequest"
+      });
+      if(didCreate.status != 200) {
+        app.log.error("Could not create card " + didCreate.status);
+        return;
+      }
+    }
+  });
 }
